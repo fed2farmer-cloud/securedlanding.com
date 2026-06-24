866bc28531b9537211e1bad31acee4c9c1a5dff0
fed2farmer-cloud <fed2farmer@gmail.com>
2026-06-24 07:38:32 -0400

    Update Invest.tsx

--- Invest.tsx
+++ Invest.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import Reveal from "./Reveal";
-import { useCountUp } from "../hooks/useCountUp";
+import { useCountUp } from "./useCountUp";
 
 const audiences = [
   {
