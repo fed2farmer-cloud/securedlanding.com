7be6e88f36c1e80c11c84b13dabb1c571e61371a
fed2farmer-cloud <fed2farmer@gmail.com>
2026-06-24 03:45:18 -0500

    Refactor import paths in App.tsx

--- App.tsx
+++ App.tsx
@@ -1,14 +1,14 @@
 import Nav from "./Nav";
-import Hero from "./components/Hero";
-import Marquee from "./components/Marquee";
-import ProblemSolution from "./components/ProblemSolution";
-import HowItWorks from "./components/HowItWorks";
-import Calculator from "./components/Calculator";
-import Marketplace from "./components/Marketplace";
-import Invest from "./components/Invest";
-import Security from "./components/Security";
-import FAQ from "./components/FAQ";
-import Footer from "./components/Footer";
+import Hero from "./Hero";
+import Marquee from "./Marquee";
+import ProblemSolution from "./ProblemSolution";
+import HowItWorks from "./HowItWorks";
+import Calculator from "./Calculator";
+import Marketplace from "./Marketplace";
+import Invest from "./Invest";
+import Security from "./Security";
+import FAQ from "./FAQ";
+import Footer from "./Footer";
 
 export default function App() {
   return (
