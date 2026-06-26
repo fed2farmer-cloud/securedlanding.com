cab047b9ea41cae3a7615b18fbadde0b832d8e5a
fed2farmer-cloud <fed2farmer@gmail.com>
2026-06-26 05:52:12 -0400

    Update main.tsx

--- main.tsx
+++ main.tsx
@@ -4,8 +4,13 @@ import "./index.css";
 import { ClerkProvider } from "@clerk/clerk-react";
 import App from "./App";
 
+const PUBLISHABLE_KEY =
+  "pk_test_Y29uY3JldGUtZG9scGhpbi04Ni5jbGVyay5hY2NvdW50cy5kZXYk";
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <App />
+    </ClerkProvider>
   </StrictMode>
 );
